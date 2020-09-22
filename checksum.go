9dd328386db28f1fdc10f591af9135ff0c5bddc8
Dmitry Savvateev <dsv@eml.cc>
2020-09-22 21:26:52 +0300

    check parsing errors

--- checksum.go
+++ checksum.go
@@ -22,19 +22,39 @@ func check(e error, code int) {
 	}
 }
 
+func parseLine(line string) (checksum, file string) {
+	fields := strings.SplitN(line, separator, 2)
+	if len(fields) != 2 {
+		fmt.Printf("Invalid input line: '%s'\n", line)
+		os.Exit(5)
+	}
+
+	checksum, file = fields[0], fields[1]
+	if len(checksum) == 0 {
+		fmt.Printf("Invalid input - checksum is missing: '%s'\n", line)
+		os.Exit(5)
+	}
+	if len(file) == 0 {
+		fmt.Printf("Invalid input - file path is missing: '%s'\n", line)
+		os.Exit(5)
+	}
+	return
+}
+
 func readData(file string) {
 	_, err := os.Stat(file)
 	if os.IsNotExist(err) {
+		fmt.Println("File not found:", file)
 		return // input file not found, ok
 	}
 
 	f, err := os.Open(file)
 	check(err, 3)
+	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		fields := strings.SplitN(scanner.Text(), separator, 2)
-		checksum, file := fields[0], fields[1]
+		checksum, file := parseLine(scanner.Text())
 		data[file] = checksum // TODO: check for duplicate keys
 	}
 	err = scanner.Err()
