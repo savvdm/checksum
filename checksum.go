44ad848c27b7a0e082612eec9da790106fa81d75
Dmitry Savvateev <dsv@eml.cc>
2020-09-23 21:10:42 +0300

    browse files under the specified dir
    for each file, a relative path is returned

--- checksum.go
+++ checksum.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 var data map[string]string
+var errorCount int
 
 const separator = "  " // Two-space separator used by sha1sum on Linux
 
@@ -61,6 +63,48 @@ func readData(file string) {
 	check(err, 4)
 }
 
+func makePath(path, name string) string {
+	switch {
+	case len(path) == 0:
+		return name
+	case strings.HasSuffix(path, "/"):
+		return path + name
+	default:
+		return path + "/" + name
+	}
+}
+
+func readDir(root string, prefix string, callback func(path string)) {
+	f, err := os.Open(root)
+	if err != nil {
+		fmt.Println(err)
+		errorCount++
+		return
+	}
+	defer f.Close()
+
+	const buflen = 100
+
+	info, err := f.Readdir(buflen)
+	for err == nil {
+		for _, file := range info {
+			name := file.Name()
+			path := makePath(prefix, name)
+			if file.IsDir() {
+				subdir := makePath(root, name)
+				readDir(subdir, path, callback) // prefix current path
+			} else {
+				callback(path)
+			}
+		}
+		info, err = f.Readdir(buflen)
+	}
+	if err != io.EOF {
+		fmt.Println(err)
+		errorCount++
+	}
+}
+
 func main() {
 
 	if len(os.Args) < 2 {
@@ -71,6 +115,18 @@ func main() {
 	data = make(map[string]string)
 
 	readData(os.Args[1])
-
 	fmt.Printf("Read %d checksums\n", len(data))
+
+	root := "."
+	if len(os.Args) > 2 {
+		root = os.Args[2]
+	}
+
+	count := 0
+	readDir(root, "", func(path string) {
+		fmt.Println(path)
+		count++
+	})
+
+	fmt.Printf("%s: %d files, %d errors\n", root, count, errorCount)
 }
